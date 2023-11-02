059da548abfcf034476e2a33df9431f7f1d53a58
Zero <67668275+Zero-ruan@users.noreply.github.com>
2023-11-02 10:43:36 +0800

    Update useInteractions.ts

--- Sentry/useSentry/useInteractions.ts
+++ Sentry/useSentry/useInteractions.ts
@@ -39,7 +39,6 @@ function useInteractions() {
 
     if (replay) {
       console.log("start");
-      //   await replay.flush();
       //   await replay.setupOnce();
       await replay.startBuffering();
     } else {
@@ -81,7 +80,7 @@ function useInteractions() {
         // Performance Monitoring
         tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
         // Session Replay
-        replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        // replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       });
       console.log("onRegisterSentry");
